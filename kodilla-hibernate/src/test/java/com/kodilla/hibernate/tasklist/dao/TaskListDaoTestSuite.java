aaa0dbd530d458d849dd202915daaf20c214bb88
SlawomirJablonski <forest19@interia.pl>
2019-01-16 19:19:53 +0100

    ex17.3 /added additional test cases to test scenario/

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -20,21 +20,31 @@ public class TaskListDaoTestSuite {
     public void testFindByListName() {
         //Given
         TaskList taskList = new TaskList("dreams", "New Year's resolutions");
+        TaskList taskList1 = new TaskList("achievements", "Last Year's resolutions");
         taskListDao.save(taskList);
+        taskListDao.save(taskList1);
         String questListName = taskList.getListName();
+        String questListName1 = taskList1.getListName();
 
         //When
         List<TaskList> questList = taskListDao.findByListName(questListName);
         String nameOfQuestList = questList.get(0).getListName();
         String descriptionOfQuestList = questList.get(0).getDescription();
+        String descriptionOfQuestList1 = questList.get(1).getDescription();
+        int dreamsId = questList.get(0).getId();
+        int achievementsId = questList.get(1).getId();
 
         //Then
-        Assert.assertEquals(1,questList.size());
+        Assert.assertEquals(2,questList.size());
         Assert.assertEquals("dreams",nameOfQuestList);
         Assert.assertEquals("New Year's resolutions",descriptionOfQuestList);
+        Assert.assertNotEquals("New Year's resolutions",descriptionOfQuestList1);
+        Assert.assertNotEquals(dreamsId,achievementsId);
+        Assert.assertTrue(questListName.equals(questListName1) && questListName1.equals(questListName));
+        Assert.assertEquals(questListName.hashCode(), questListName1.hashCode());
 
         //CleanUp
-        int id = questList.get(0).getId();
-        taskListDao.delete(id);
+        //int id = questList.get(0).getId();
+        taskListDao.deleteAll();
     }
 }
