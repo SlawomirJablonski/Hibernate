6f52f651095f28a34f279d93e60fb74cea77ad37
SlawomirJablonski <forest19@interia.pl>
2019-01-25 11:21:07 +0100

    cleanUp tasks to separate method

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -2,7 +2,9 @@ package com.kodilla.hibernate.manytomany.dao;
 
 import com.kodilla.hibernate.manytomany.Company;
 import com.kodilla.hibernate.manytomany.Employee;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,17 +22,29 @@ public class CompanyDaoTestSuite {
     @Autowired
     EmployeeDao employeeDao;
 
+    private Employee johnSmith = new Employee("John", "Smith");
+    private Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+    private Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+    private Company softwareMachine = new Company("Software Machine");
+    private Company dataMaesters = new Company("Data Maesters");
+    private Company greyMatter = new Company("Grey Matter");
+
+    private int softwareMachineId = softwareMachine.getId();
+    private int dataMaestersId = dataMaesters.getId();
+    private int greyMatterId = greyMatter.getId();
+
+    private Company c1 = new Company("coca-cola");
+    private Company c2 = new Company("colgate");
+    private Company c3 = new Company("coccodrillo");
+
+    private Employee e1 = new Employee("Hubert", "Głowacki");
+    private Employee e2 = new Employee("Magdalena", "Kamińska");
+    private Employee e3 = new Employee("Michał", "Mazurkiewicz");
+
     @Test
     public void testSaveManyToMany() {
         //Given
-        Employee johnSmith = new Employee("John", "Smith");
-        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
-        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
-
-        Company softwareMachine = new Company("Software Machine");
-        Company dataMaesters = new Company("Data Maesters");
-        Company greyMatter = new Company("Grey Matter");
-
         softwareMachine.getEmployees().add(johnSmith);
         dataMaesters.getEmployees().add(stephanieClarckson);
         dataMaesters.getEmployees().add(lindaKovalsky);
@@ -45,38 +59,18 @@ public class CompanyDaoTestSuite {
 
         //When
         companyDao.save(softwareMachine);
-        int softwareMachineId = softwareMachine.getId();
         companyDao.save(dataMaesters);
-        int dataMaestersId = dataMaesters.getId();
         companyDao.save(greyMatter);
-        int greyMatterId = greyMatter.getId();
 
         //Then
         Assert.assertNotEquals(0, softwareMachineId);
         Assert.assertNotEquals(0, dataMaestersId);
         Assert.assertNotEquals(0, greyMatterId);
-
-        //CleanUp
-        //try {
-        //    companyDao.delete(softwareMachineId);
-        //    companyDao.delete(dataMaestersId);
-        //    companyDao.delete(greyMatterId);
-        //} catch (Exception e) {
-        //    //do nothing
-        //}
     }
 
     @Test
     public void testNamedNativeQuery() {
         //Given
-        Company c1 = new Company("coca-cola");
-        Company c2 = new Company("colgate");
-        Company c3 = new Company("coccodrillo");
-
-        Employee e1 = new Employee("Hubert", "Głowacki");
-        Employee e2 = new Employee("Magdalena", "Kamińska");
-        Employee e3 = new Employee("Michał", "Mazurkiewicz");
-
         List<Company> e1Companies = new ArrayList<>();
         e1Companies.add(c1);
         e1Companies.add(c2);
@@ -114,30 +108,14 @@ public class CompanyDaoTestSuite {
         Employee firstEmployee2 = firstCompanyOnTheList2.getEmployees().get(0);
 
         //Then
-        Assert.assertEquals("Głowacki",firstEmployee.getLastname());
-        Assert.assertEquals("Hubert",firstEmployee2.getFirstname());
-        Assert.assertEquals(1,companies.size());
-
-        //CleanUp
-        try {
-            companyDao.delete(c1);
-            companyDao.delete(c2);
-            companyDao.delete(c3);
-        } catch (Exception e) {
-            //do nothing
-        }
+        Assert.assertEquals("Głowacki", firstEmployee.getLastname());
+        Assert.assertEquals("Hubert", firstEmployee2.getFirstname());
+        Assert.assertEquals(1, companies.size());
     }
+
     @Test
     public void testNamedQuery() {
         //Given
-        Company c1 = new Company("coca-cola");
-        Company c2 = new Company("colgate");
-        Company c3 = new Company("coccodrillo");
-
-        Employee e1 = new Employee("Hubert", "Głowacki");
-        Employee e2 = new Employee("Magdalena", "Kamińska");
-        Employee e3 = new Employee("Michał", "Mazurkiewicz");
-
         List<Company> e1Companies = new ArrayList<>();
         e1Companies.add(c1);
         e1Companies.add(c2);
@@ -170,10 +148,30 @@ public class CompanyDaoTestSuite {
         Employee firstMatchingEmployee = employeesWithMatchingName.get(0);
 
         //Then
-        Assert.assertEquals("Magdalena",firstMatchingEmployee.getFirstname());
-        Assert.assertEquals(1,employeesWithMatchingName.size());
+        Assert.assertEquals("Magdalena", firstMatchingEmployee.getFirstname());
+        Assert.assertEquals(1, employeesWithMatchingName.size());
+    }
 
+    @After
+    public void cleanUp() {
         //CleanUp
+        try {
+            companyDao.delete(softwareMachineId);
+            companyDao.delete(dataMaestersId);
+            companyDao.delete(greyMatterId);
+        } catch (
+                Exception e) {
+            //do nothing
+        }
+
+        try {
+            companyDao.delete(c1);
+            companyDao.delete(c2);
+            companyDao.delete(c3);
+        } catch (Exception e) {
+            //do nothing
+        }
+
         try {
             employeeDao.delete(e1);
             employeeDao.delete(e2);
